4fca04d6518b7bd09fe0e755ab5a1a8e81c90ae5
Uros Stanimirovic <urosstan3.0@gmail.com>
2023-01-05 23:12:09 +0100

    feat: Adding item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit{
+ @ViewChild('nameInput',{static: true}) nameInputReference: ElementRef;
+ @ViewChild('amountInput',{static: true}) amountInputReference: ElementRef;
+ @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(){
+    const ingredient = new Ingredient(this.nameInputReference.nativeElement.value, this.amountInputReference.nativeElement.value)
+    this.ingredientAdded.emit(ingredient)
+  }
+
+  ngOnInit(): void {
+  }
 }
