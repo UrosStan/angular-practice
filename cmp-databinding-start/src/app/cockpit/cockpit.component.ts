9292b5d06c739fc829b1eef5eb26f2271f4a3e83
Uros Stanimirovic <urosstan3.0@gmail.com>
2023-01-05 15:14:03 +0100

    feat: References with viewChild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -12,7 +12,7 @@ export class CockpitComponent implements OnInit {
   blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
   // newServerName = 'Predefined newServerName';
-  newServerContent = 'Predefined newServerContent';
+ @ViewChild('serverContentReference') newServerContent: ElementRef; 
 
   constructor() { }
 
@@ -21,11 +21,11 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.newServerContent.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
+    this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent.nativeElement.value });
   }
 }
